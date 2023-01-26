2bafe30344eb3cfa4b34d78fe3fef0097d5fb1da
ashrafulabir1 <ashrafulabir.nsu@gmail.com>
2023-01-27 00:37:55 +0600

    first commit

---
+++ src/StringIssue.java
@@ -0,0 +1,21 @@
+
+public class StringIssue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name= "";
+		System.out.println(stringIssue(name));
+	}
+
+	public static String stringIssue(String firstName) {
+		String lastName = "Abir";
+		firstName="Ashraful";
+		if ("Ashraful" =="Abir") {
+			return firstName;
+		}
+		else {
+		return lastName;
+		}
+	}
+}
